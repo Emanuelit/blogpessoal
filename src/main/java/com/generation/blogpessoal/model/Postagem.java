e90af04cd63ef6db9aafc8f409e65393502ccb90
Emanuelit <emanuelitavelino@gmail.com>
2022-03-24 11:57:05 -0300

    CRUD parte 01

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,67 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagens") // CREATE TABLE tb_postagens(
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	private Long id;
+
+	@NotBlank(message = "O atributo título é Obrigatório: ")
+	@Size(min = 5, max = 100, message = " O atributo título deve possuir no mínimo 5 e no máximo 100 carácteres ")
+	private String titulo;
+
+	@NotBlank(message = "O atributo texto é Obrigatório: ")
+	@Size(min = 10, max = 1000, message = " O atributo texto deve possuir no mínimo 10 e no máximo 1000 carácteres ")
+	private String texto;
+
+	@UpdateTimestamp
+	private LocalDate data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+}
---
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package com.generation.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.blogpessoal.model.Postagem;
+
+//Long está recebendo o objeto da classe postagem e também o identificador único da tabela Primary key Long
+//jpa repository tem todos os métodos para trabalhar com o banco de dados
+//método find, save, delete
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+//métodos para implementar interface
+
+}
